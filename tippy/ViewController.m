647a7d97ab3ed34986ac53976fc01a2d0492ed6e
José Antonio Murillo <josemurillo@fb.com>
2019-06-25 15:47:02 -0700

    Added first features of the app

--- tippy/ViewController.m
+++ tippy/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+
 @end
 
 @implementation ViewController
@@ -19,5 +23,18 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing:YES];
+}
+- (IBAction)onEdit:(id)sender {
+    
+    double bill = [self.billField.text doubleValue];
+    double tip = bill * 0.2;
+    double total = 
+    
+    self.tipLabel.text = @"$10.00";
+    self.totalLabel.text = @"$10.00";
+}
 
 @end
