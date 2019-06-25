dbdf00e929b32b9edeae577813b0cd1414a803af
José Antonio Murillo <josemurillo@fb.com>
2019-06-25 14:09:21 -0700

    Initial Commit

---
+++ tippy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tippy
+//
+//  Created by josemurillo on 6/25/19.
+//  Copyright © 2019 josemurillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tippy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tippy
+//
+//  Created by josemurillo on 6/25/19.
+//  Copyright © 2019 josemurillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
