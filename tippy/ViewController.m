ac52a967119559bfbf429bb8c8f602b611140ae8
José Antonio Murillo <josemurillo@fb.com>
2019-06-25 16:16:48 -0700

    Finished basic functionality of Tippy app

--- tippy/ViewController.m
+++ tippy/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *billField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -29,12 +30,16 @@
 }
 - (IBAction)onEdit:(id)sender {
     
-    double bill = [self.billField.text doubleValue];
-    double tip = bill * 0.2;
-    double total = 
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
     
-    self.tipLabel.text = @"$10.00";
-    self.totalLabel.text = @"$10.00";
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double bill = [self.billField.text doubleValue];
+    double tip = bill * tipPercentage;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
 
 @end
